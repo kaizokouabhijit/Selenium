904f04b2786aedeae7ac72373ee37413a572737d
Abhijit Shubham <31901950+kaizokouabhijit@users.noreply.github.com>
2022-09-07 20:05:35 +0530

    new commit

--- SELENIUM/Page object Model/src/com/easycalculationpages/Main.java
+++ SELENIUM/Page object Model/src/com/easycalculationpages/Main.java
@@ -2,6 +2,7 @@ package com.easycalculationpages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -13,18 +14,28 @@ public class Main {
 	
 	@BeforeTest
 	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhijit.s\\OneDrive - SintecMedia Ltd\\Desktop\\SELENIUM\\Page object Model\\ChromeBrowser\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhijit.s\\OneDrive - SintecMedia Ltd\\Desktop\\git\\Selenium\\Selenium\\SELENIUM\\Page object Model\\ChromeBrowser\\chromedriver.exe");
 		driver = new ChromeDriver();
-		
-		
-		
-	@Test
-		public void navigated()
-		{
 		driver.get("https://www.nopcommerce.com/en/login?returnUrl=%2Fen");
+		
+	}
+	
+	@Test
+	
+	public void navigat() throws InterruptedException {
 		csp = new casestudypom(driver);
-		csp.login("Abhijit", "passworD@123");
-		}
+		csp.login("Abhijit", "Password@123");
+		
+		Thread.sleep(8000);
+		
+	}
+	
+	
+	
+	@AfterTest
+	
+	public void wrapUp() {
+		driver.quit();
 	}
 
 }
--- SELENIUM/Page object Model/src/com/easycalculationpages/casestudypom.java
+++ SELENIUM/Page object Model/src/com/easycalculationpages/casestudypom.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeTest;
 
 public class casestudypom {
@@ -11,6 +12,7 @@ public class casestudypom {
 	WebDriver driver;
 	public casestudypom(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 		
 	}
 	
